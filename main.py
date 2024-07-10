0827c43f0de9981892a9b45800e82e1d4e16aafb
404EDEN404 <laurenshill@outlook.com>
2024-07-10 16:36:54 +0200

    Revert "correct version"
    This reverts commit bdebf4d3d01bf28bca45c4b17d1f334b2c290ad9.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# This is the main python file to run the game
 from tkinter import *
 import random
 
@@ -10,75 +11,93 @@ SNAKE_COLOR = '#00FF00'
 FOOD_COLOR = '#FF0000'
 BACKGROUND_COLOR = '#000000'
 
-class Snake:
+
+class Snake():
+
     def __init__(self):
         self.body_size = BODY_PARTS
         self.coordinates = []
         self.squares = []
 
         for i in range(0, BODY_PARTS):
-            self.coordinates.append([0, 0])
+            self.coordinates.append([0,0])
 
         for x, y in self.coordinates:
-            square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR, tags="snake")
+            square = canvas.create_rectangle(x,y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR, tags="Snake")
             self.squares.append(square)
 
-class Food:
+class Food():
     def __init__(self):
-        x = random.randint(0, (GAME_WIDTH // SPACE_SIZE) - 1) * SPACE_SIZE
-        y = random.randint(0, (GAME_HEIGHT // SPACE_SIZE) - 1) * SPACE_SIZE
-        self.coordinates = [x, y]
-        self.food_item = canvas.create_rectangle(x, y, x + SPACE_SIZE - 10, y + SPACE_SIZE - 10, fill=FOOD_COLOR, tags="food")
+        x = random.randint(0, int(GAME_WIDTH / SPACE_SIZE-1) * SPACE_SIZE)
+        y = random.randint(0, int(GAME_HEIGHT / SPACE_SIZE - 1) * SPACE_SIZE)
 
+        self.coordinates = [x,y]
+
+        canvas.create_rectangle(x, y, x + SPACE_SIZE-10, y + SPACE_SIZE-10, fill=FOOD_COLOR, tags="food")
 def next_turn(snake, food):
-    x, y = snake.coordinates[0]
 
+    x,y = snake.coordinates[0]
     if direction == "up":
-        y -= SPACE_SIZE
+        y-= SPACE_SIZE
     elif direction == "down":
-        y += SPACE_SIZE
+        y+= SPACE_SIZE
     elif direction == "left":
-        x -= SPACE_SIZE
+        x-= SPACE_SIZE
     elif direction == "right":
-        x += SPACE_SIZE
+        x+= SPACE_SIZE
 
-    snake.coordinates.insert(0, (x, y))
+    snake.coordinates.insert(0, (x,y))
 
-    square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR)
+    square = canvas.create_rectangle(x,y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR)
+
     snake.squares.insert(0, square)
 
     if x == food.coordinates[0] and y == food.coordinates[1]:
+
         global score
+
         score += 1
+
         label.config(text="Score:{}".format(score))
+
         canvas.delete("food")
+
         food = Food()
+
     else:
         del snake.coordinates[-1]
+
         canvas.delete(snake.squares[-1])
+
         del snake.squares[-1]
 
     window.after(SPEED, next_turn, snake, food)
-
 def change_direction(new_direction):
+
     global direction
 
-    if new_direction == "left" and direction != "right":
-        direction = new_direction
-    elif new_direction == "right" and direction != "left":
-        direction = new_direction
-    elif new_direction == "up" and direction != "down":
-        direction = new_direction
-    elif new_direction == "down" and direction != "up":
-        direction = new_direction
+    if new_direction == "left":
+        if direction != "right":
+            direction = new_direction
 
+    if new_direction == "right":
+        if direction != "left":
+            direction = new_direction
+
+    if new_direction == "up":
+        if direction != "down":
+            direction = new_direction
+
+    if new_direction == "down":
+        if direction != "up":
+            direction = new_direction
 def check_collisions(snake):
     pass
 
 def game_over():
     pass
 
-window = Tk()
+window =Tk()
 window.title("Snake Game!")
 window.resizable(False, False)
 
@@ -98,8 +117,8 @@ window_height = window.winfo_height()
 screen_width = window.winfo_screenwidth()
 screen_height = window.winfo_screenheight()
 
-x = int((screen_width / 2) - (window_width / 2))
-y = int((screen_height / 2) - (window_height / 2))
+x = int((screen_width/2) - (window_width/2))
+y = int((screen_height/2) - (window_height/2))
 
 window.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
@@ -107,10 +126,13 @@ for key in ('<Left>', 'a', 'A'): window.bind(key, lambda event: change_direction
 for key in ('<Right>', 'd', 'D'): window.bind(key, lambda event: change_direction('right'))
 for key in ('<Up>', 'w', 'W'): window.bind(key, lambda event: change_direction('up'))
 for key in ('<Down>', 's', 'S'): window.bind(key, lambda event: change_direction('down'))
+# window.bind('<Right>', lambda event : change_direction('right'))
+# window.bind('<Up>', lambda event : change_direction('up'))
+# window.bind('<Down>', lambda event : change_direction('down'))
 
 snake = Snake()
 food = Food()
 
-next_turn(snake, food)
+next_turn(snake,food)
 
 window.mainloop()
